65e7cf741a09134b368fc97c43e40158f7d8d2c7
lesOlesya <Olesyad285@gmail.com>
2023-08-12 12:55:04 +0300

    9_sprint/3_homework

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,60 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val backButton = findViewById<ImageButton>(R.id.search_back)
+        val editText = findViewById<EditText>(R.id.edit_text)
+        val clearButton = findViewById<ImageView>(R.id.clear_icon)
+
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        clearButton.setOnClickListener {
+            editText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        editText.addTextChangedListener(simpleTextWatcher)
+
+//        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+//        inputMethodManager?.hideSoftInputFromWindow(`currentView`.windowToken, 0)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,20 +1,47 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageButton
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
         val backButton = findViewById<ImageButton>(R.id.settings_back)
-
         backButton.setOnClickListener {
             finish()
+        }
+
+        val shareButton = findViewById<FrameLayout>(R.id.share_app)
+        shareButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.link_YP_android))
+            startActivity(intent)
+        }
+
+        val supportButton = findViewById<FrameLayout>(R.id.support)
+        supportButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email)))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject_YP))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.message_YP))
+            startActivity(intent)
+        }
+
+        val termsOfUseButton = findViewById<FrameLayout>(R.id.terms_of_use)
+        termsOfUseButton.setOnClickListener {
+            val url = Uri.parse(getString(R.string.link_YP_terms_of_use))
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(intent)
         }
     }
 }
