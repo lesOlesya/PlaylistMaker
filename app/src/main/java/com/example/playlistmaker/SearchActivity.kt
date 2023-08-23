6f3b213adef2b6f39a24e8ecef563152e956851c
lesOlesya <Olesyad285@gmail.com>
2023-08-23 06:27:59 +0300

    11_sprint

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -7,34 +7,55 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SearchActivity : AppCompatActivity() {
+
+    private val iTunesSearchBaseUrl = "https://itunes.apple.com"
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesSearchBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val iTunesSearchService = retrofit.create(ITunesSearchApi::class.java)
+
+    private val tracks = ArrayList<Track>()
+    private val adapter = TrackAdapter(tracks)
+
     private var searchText = ""
+
     private lateinit var editText: EditText
-    private val tracks = arrayListOf(
-        Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-        Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-        Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-        Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-        Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-    )
-    @SuppressLint("MissingInflatedId")
+    private lateinit var nothingFound: LinearLayout
+    private lateinit var notInternet: LinearLayout
+    @SuppressLint("MissingInflatedId", "NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val backButton = findViewById<ImageButton>(R.id.search_back)
         editText = findViewById(R.id.edit_text)
+        nothingFound = findViewById(R.id.llNothingFound)
+        notInternet = findViewById(R.id.llNotInternet)
         val clearButton = findViewById<ImageView>(R.id.clear_icon)
+        val updateButton = findViewById<Button>(R.id.button_update)
         val rvTracks = findViewById<RecyclerView>(R.id.rvTracks)
 
-        rvTracks.adapter = TrackAdapter(tracks)
+        rvTracks.adapter = adapter
 
         backButton.setOnClickListener {
             finish()
@@ -45,6 +66,12 @@ class SearchActivity : AppCompatActivity() {
             val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
             editText.clearFocus()
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+        }
+
+        updateButton.setOnClickListener {
+            findTracks()
         }
 
         editText.doOnTextChanged { s, start, before, count ->
@@ -52,6 +79,65 @@ class SearchActivity : AppCompatActivity() {
             clearButton.visibility = clearButtonVisibility(s)
         }
 
+        editText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                if (editText.text.isNotEmpty()) {
+                    findTracks()
+                }
+                true
+            }
+            false
+        }
+
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun showMessage(text: String) {
+        if (text == "nothing found") {
+            notInternet.visibility = View.GONE
+            nothingFound.visibility = View.VISIBLE
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+
+        } else if (text == "not internet"){
+            nothingFound.visibility = View.GONE
+            notInternet.visibility = View.VISIBLE
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+        } else {
+            nothingFound.visibility = View.GONE
+            notInternet.visibility = View.GONE
+        }
+    }
+
+    private fun findTracks() {
+        iTunesSearchService.search(editText.text.toString()).enqueue(object :
+            Callback<TracksResponse> {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onResponse(call: Call<TracksResponse>,
+                                    response: Response<TracksResponse>
+            ) {
+                if (response.code() == 200) {
+                    tracks.clear()
+                    if (response.body()?.results?.isNotEmpty() == true) {
+                        tracks.addAll(response.body()?.results!!)
+                        adapter.notifyDataSetChanged()
+                    }
+                    if (tracks.isEmpty()) {
+                        showMessage("nothing found")
+                    } else {
+                        showMessage("")
+                    }
+                } else {
+                    showMessage("not internet")
+                }
+            }
+
+            override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                showMessage("not internet")
+            }
+
+        })
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
